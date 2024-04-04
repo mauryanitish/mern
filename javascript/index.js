1250f34f5f726068a438742f18a2eca4dedbf8af
mauryanitish <m.nitishkumar786@gmail.com>
2024-04-04 21:07:40 +0530

    first day

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+console.log("Hello world");
+console.warn("Hello world");
+console.error("Hello world");
+
+// show on front of user 
+document.write("Hello wrold.")
+
+// show pop up
+alert("Hello world!")
